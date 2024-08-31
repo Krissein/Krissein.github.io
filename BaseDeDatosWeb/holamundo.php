23e876478cdb3a93dc621d6cf8c7bc0a634fd6b5
Kris <159452424+Krissein@users.noreply.github.com>
2024-08-30 18:45:26 -0700

    hola mundo php 30/08/24

---
+++ BaseDeDatosWeb/holamundo.php
@@ -0,0 +1,14 @@
+<?php
+
+$nombre="Kris";
+$numero=4;
+$numero2="4";
+$total=$numero+$numero2;
+echo "total =" . total;
+echo "<h1> ola Matías </h1>";
+for($=i; $=<10; $i++){
+    echo $i."<br>";
+}
+echo"<h2> Kris </h2>"
+
+?>
